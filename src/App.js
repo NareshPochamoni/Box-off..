16b2fede43ff51b7ebea4571cc4538b2877322fe
NareshPochamon <nareshpochamoni1234@gmail.com>
2021-12-10 01:56:53 +0530

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 /* eslint-disable react/function-component-definition */
  import React from 'react';
+ import {Switch,Route} from 'react-router-dom';
+
 
 function App() {
-  return (
-    <div className="App">
-       hi hello
-    </div>
-  );
+  return  <Switch>
+
+    <Route exact path="/">
+      This is home page
+    </Route>
+    <Route exact path="/starred">
+      This is starred page
+    </Route>
+    <Route  >
+      This is 404 page
+    </Route>
+  </Switch>
 }
 
 export default App;
