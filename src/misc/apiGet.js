4b083200f6832e956774c7a04953906985fa5239
NareshPochamon <nareshpochamoni1234@gmail.com>
2021-12-11 00:20:17 +0530

    updated show page

--- src/misc/apiGet.js
+++ src/misc/apiGet.js
@@ -2,6 +2,7 @@ const API_BASE_URL='https://api.tvmaze.com';
 
 export async function apiGet(query){
     const response=await fetch(`${API_BASE_URL}${query}`).then(r=>r.json());
+     
     return response;
 
 }
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-const-assign */
 /* eslint-disable react/function-component-definition */
 import React,{useEffect,useState} from 'react';
 import {useParams} from 'react-router-dom';
@@ -6,20 +7,50 @@ import { apiGet } from '../misc/apiGet';
 const Show = () => {
     const {id}=useParams();
      const [show,setShow]=useState(null);
+     const [isLoading,setIsLoading]=useState(true);
+     const [error,setError]=useState(null);
     useEffect(()=>{
 
+        let isMounted=true;
+
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results=>{
+
+            
+            if(isMounted){
                 setShow(results);
-        })
+                setIsLoading(false);}
+           
+            
+                
+        }).catch(err=>{
+            if(isMounted){
+            setError(err.message);
+            setIsLoading(false);
+            }
+        });
+
+        
+        
+        return ()=>{
+            isMounted=false;
+        }
         
 
     },[id]);
     console.log('show',show);
+
+    if (isLoading){
     return (
         <div>
-            This is Show me page
+            Data is Being Loaded
         </div>
     )
+    }
+    if(error)
+    {
+            return <div>Error occured :{error}</div>
+    }
+    return <div>This is Show page</div>
 }
 
 export default Show;
