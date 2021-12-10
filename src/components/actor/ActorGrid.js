5c1d8ff3aeebcb73e969f0671ffd0b064a8c5a1b
NareshPochamon <nareshpochamoni1234@gmail.com>
2021-12-10 22:09:59 +0530

    Styled Components

--- src/components/actor/ActorGrid.js
+++ src/components/actor/ActorGrid.js
@@ -3,16 +3,17 @@
 import React from 'react'
 import ActorCard from './ActorCard'
 import IMG_NOT_FOUND from '../../images/not-found.png';
+import { FlexGrid } from '../styled';
 const ActorGrid = ({data}) => {
     return (
-        <div>
+        <FlexGrid>
             {
                 data.map(item=><div key={item.person.id}><ActorCard name={item.person.name} 
                     gender={item.person.gender} country={item.person.country?item.person.country.name:null} 
                     birthday={item.person.birthday} deathday={item.person.deathday} image={item.person.image?item.person.image.medium:IMG_NOT_FOUND} />
                     </div>)
             }
-        </div>
+        </FlexGrid>
     )
 }
 
--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -2,13 +2,14 @@
 import React from 'react'
 import ShowCard from './ShowCard';
 import IMG_NOT_FOUND from '../../images/not-found.png';
+import { FlexGrid } from '../styled';
 
 const ShowGrid = ({data}) => {
-    return <div>
+    return <FlexGrid>
         {
             data.map(item=><div key={item.show.id}><ShowCard id={item.show.id} name={item.show.name} image={item.show.image?item.show.image.medium:IMG_NOT_FOUND} summary={item.show.summary}/></div>)
         }
-    </div>
+    </FlexGrid>
          
     
 }
